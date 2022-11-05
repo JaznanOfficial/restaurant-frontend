df0f3214fdfebc68ec50b79f6add879f75a02988
JaznanOfficial <jaznanofficial@gmail.com>
2022-11-05 21:27:36 +0600

    navbar almost done

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./Components/Navigation/Navbar";
+
 
 const HomePage = () => {
     return (
         <div>
+            <Navbar/>
             
         </div>
     );
